90867cc20162faef5e13037ded39b77c330297e6
ComondanteChe <155365815+ComondanteChe@users.noreply.github.com>
2025-10-12 21:20:50 +0700

    Update template_file.tf

--- template_file.tf
+++ template_file.tf
@@ -2,6 +2,6 @@ data template_file "ssh_key" {
   template = file("${path.module}/cloud-init.yml")
 
     vars = {
-      ssh_key = file(var.vms_ssh_root_key)
+      ssh_key = var.vms_ssh_root_key
     }
 }
