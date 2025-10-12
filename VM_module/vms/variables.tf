02b0ed942ad70386dc9542affb4e247fd886be28
ComondanteChe <155365815+ComondanteChe@users.noreply.github.com>
2025-10-12 16:24:34 +0700

    Update variables.tf

--- VM_module/vms/variables.tf
+++ VM_module/vms/variables.tf
@@ -2,5 +2,5 @@
 
 variable "public_key" {
   type    = string
-  default = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGiVcfW8Wa/DxbBNzmQcwn7hJOj7ji9eoTpFakVnY/AI webinar"
+  default = file ("~/.ssh/id_ed25519.pub")
 }
