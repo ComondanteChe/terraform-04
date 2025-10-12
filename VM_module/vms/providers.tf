e89a6a0eeb774f971d615ceb636f2d2a35bc3a33
ComondanteChe <155365815+ComondanteChe@users.noreply.github.com>
2025-10-12 16:24:31 +0700

    Update providers.tf

--- VM_module/vms/providers.tf
+++ VM_module/vms/providers.tf
@@ -9,8 +9,8 @@ terraform {
 
 provider "yandex" {
   # token                    = "do not use!!!"
-  cloud_id                 = "b1gn3ndpua1j6jaabf79"
-  folder_id                = "b1gfu61oc15cb99nqmfe"
-  service_account_key_file = file("~/.authorized_key.json")
+  cloud_id                 = "b1g7i90u6v2v15jrqpdj"
+  folder_id                = "b1gnfat6d5tspst8frqn"
+  service_account_key_file = file("~/.ssh/.authorized_key.json")
   zone                     = "ru-central1-a" #(Optional) 
 }
