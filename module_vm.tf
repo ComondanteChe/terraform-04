074095046f0915a34892502f71772ef9df3f655a
ComondanteChe <155365815+ComondanteChe@users.noreply.github.com>
2025-10-12 19:37:32 +0700

    Update module_vm.tf

--- module_vm.tf
+++ module_vm.tf
@@ -1,17 +1,18 @@
-module "test-vm" {
+module "example-vm" {
   source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
-  env_name       = "develop" 
+  env_name       = "stage"
   network_id     = yandex_vpc_network.develop.id
-  subnet_zones   = "ru-central1-a"
-  subnet_ids     = yandex_vpc_subnet.develop.id
-  instance_name  = "webs"
+  subnet_zones   = ["ru-central1-a"]
+  subnet_ids     = [yandex_vpc_subnet.develop.id]
+  instance_name  = "web-stage"
   instance_count = 1
   image_family   = "ubuntu-2004-lts"
   public_ip      = true
 
-  labels = { 
-      project = "marketing"
-     }
+  labels = {
+    owner= "e.osipov"
+    project = "marketing"
+  }
 
   metadata = {
     serial-port-enable = 1
