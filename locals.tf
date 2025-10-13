f006e4406b3b6db5213aec38674857f7b53cd45b
ComondanteChe <155365815+ComondanteChe@users.noreply.github.com>
2025-10-13 16:57:08 +0700

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,3 +1,3 @@
 locals {
-   ssh_public_key = file("ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIfVKappDvBQ9kbqze6JrwUpNwdaS9/W8TH47whYi2ls administrator@Educational")
+   ssh_public_key = file("~/.ssh/id_ed25519.pub")
 }
