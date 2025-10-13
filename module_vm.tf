057ae527fc605ff49a2289a794e06f97deccbe3c
ComondanteChe <155365815+ComondanteChe@users.noreply.github.com>
2025-10-13 16:55:32 +0700

    Update module_vm.tf

--- module_vm.tf
+++ module_vm.tf
@@ -1,10 +1,10 @@
-module "example-vm_01" {
+module "marketing-vm" {
   source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
-  env_name       = "stage1"
+  env_name       = "marketing"
   network_id     = yandex_vpc_network.develop.id
   subnet_zones   = ["ru-central1-a"]
   subnet_ids     = [yandex_vpc_subnet.develop.id]
-  instance_name  = "web-stage1"
+  instance_name  = "web-marketing"
   instance_count = 1
   image_family   = "ubuntu-2004-lts"
   public_ip      = true
@@ -20,13 +20,13 @@ module "example-vm_01" {
 
 }
 
-module "example-vm_02" {
+module "analytics-vm" {
   source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
-  env_name       = "stage2"
+  env_name       = "analytics"
   network_id     = yandex_vpc_network.develop.id
   subnet_zones   = ["ru-central1-a"]
   subnet_ids     = [yandex_vpc_subnet.develop.id]
-  instance_name  = "web-stage2"
+  instance_name  = "web-analytics"
   instance_count = 1
   image_family   = "ubuntu-2004-lts"
   public_ip      = true
