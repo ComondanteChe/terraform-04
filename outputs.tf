51b27f703d70895f1423280e180d3c1dbbce8c73
ComondanteChe <155365815+ComondanteChe@users.noreply.github.com>
2025-10-14 10:33:37 +0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,7 @@
 output "out" {
+    value = concat(module.marketing-vm.fqdn, module.analytics-vm.fqdn)
+}
 
-    value=concat(module.marketing-vm.fqdn, module.analytics-vm.fqdn)
+output "cloud-init" {
+   value = data.template_file.ssh_key
 }
