b41bfd1c1c3ca5ac9c533fea27db171829a69cc6
ComondanteChe <155365815+ComondanteChe@users.noreply.github.com>
2025-10-14 09:33:31 +0700

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,3 +1,3 @@
-locals {
-   ssh_public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIfVKappDvBQ9kbqze6JrwUpNwdaS9/W8TH47whYi2ls administrator@Educational"
-}
+# locals {
+#    ssh_public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIfVKappDvBQ9kbqze6JrwUpNwdaS9/W8TH47whYi2ls administrator@Educational"
+# }
