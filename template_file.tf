4447a3089a471fa43c6526268300d39fae8abbae
ComondanteChe <155365815+ComondanteChe@users.noreply.github.com>
2025-10-14 23:09:22 +0700

    Update template_file.tf

--- template_file.tf
+++ template_file.tf
@@ -2,7 +2,7 @@ data template_file "ssh_key" {
   template = file("${path.module}/cloud-init.yml")
 
     vars = {
-       ssh_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIfVKappDvBQ9kbqze6JrwUpNwdaS9/W8TH47whYi2ls administrator@Educational"
+       ssh_public_key = file(var.ssh_public_key)
        HOSTNAME = "${var.vm_web_name}"
     }
 }
