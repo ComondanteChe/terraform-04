d939fd24f78168727432750fb531b776390078a5
ComondanteChe <membasa1@gmail.com>
2025-10-22 14:57:40 +0000

    template_file deleted

--- template_file.tf
+++
@@ -1,9 +0,0 @@
-# data template_file "ssh_key" {
-#   template = file("${path.module}/cloud-init.yml")
-
-#     vars = {
-#        ssh_public_key = var.ssh_public_key
-#        HOSTNAME = var.vm_web_name
-#     }
-# }
-
